deaf614b96477a90af18eaffc925722856f7bcd1
Volodymyr Chornonoh <chernonog.vova@gmail.com>
2025-04-21 17:29:48 +0300

    chore: update page title, readme, config port

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
 import "./index.css";
 import { Tracks } from "./pages/Tracks";
 import { Toaster } from "./components/ui/sonner";
@@ -10,6 +10,10 @@ const queryClient = new QueryClient();
 const router = createBrowserRouter([
   {
     path: "/",
+    Component: () => <Navigate to="/tracks" />,
+  },
+  {
+    path: "/tracks",
     Component: Tracks,
   },
 ]);
